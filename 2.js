6d8cac3d6b3204cd7f6825788b1bcae1e1e5bc3f
Elena Gerasimovich <nararock@mail.ru>
2022-12-10 22:16:22 +0300

    fixed 2.js

--- 2.js
+++ 2.js
@@ -2,5 +2,5 @@ function greeting(your_name) {
     console.log(`Приветствую тебя, ${your_name}!`);
 }
 
-const name_enter = prompt("Ваше имя?");
+const name_enter = prompt(`Ваше имя?`);
 greeting(name_enter);
